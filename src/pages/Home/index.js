839a01a18bd2a1212bb7c6366a761998b053d3df
danielVFS <danielv759153@gmail.com>
2020-08-20 16:50:53 -0300

    styling catalog options

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,65 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Home() {
   return (
-    <View>
-      <Text style={{marginTop: 50, fontFamily: 'Anton_400Regular'}}>Home</Text>
+    <View style={styles.container}>  
+      <View style={styles.header}>
+        <Image 
+          source={require('../../assets/banner.png')}
+          style={styles.headerImage}
+        />
+      </View>
+
+      <View style={styles.optionsCatalogContainer}>
+        <Text style={styles.optionsCatalogText}>TÊNIS</Text>
+        <Text style={[styles.optionsCatalogText, {color: '#cececf'}]}>●</Text>
+        <Text style={[styles.optionsCatalogText, {color: '#cececf'}]}>MASCULINO</Text>
+        <TouchableOpacity style={styles.filterButton}>
+          <MaterialIcons 
+            name="filter-list"
+            size={24}
+            color="#000"
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.line}/>
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#FFF',
+  },
+  header: {
+    marginBottom: 8,
+  },
+  headerImage: {
+    width: '100%',
+  },
+  filterButton: {
+    position: 'absolute',
+    right: 0,
+    alignSelf: 'center',
+  },
+  optionsCatalogContainer: {
+    flexDirection: 'row',
+    marginVertical: '5%',
+    marginHorizontal: '5%'
+  },
+  optionsCatalogText: {
+    fontFamily: 'Anton_400Regular',
+    fontSize: 26,
+    marginHorizontal: '1%'
+  },
+  line: {
+    borderBottomColor: '#d8d8d8',
+    borderBottomWidth: 2,
+  }
+});
