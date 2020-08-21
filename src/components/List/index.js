0ffda85a2500a0000df31fcb6a0df0d66998ea06
danielVFS <danielv759153@gmail.com>
2020-08-21 17:15:20 -0300

    creating the list of items

---
+++ src/components/List/index.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native'
+
+export default function List({ img, cost, children }) {
+
+  function filterDesc(desc){
+    if(desc.length < 20){
+        return desc;
+    }
+
+    return `${desc.substring(0, 20)}...`;
+ }
+  
+  return (
+   <TouchableOpacity style={styles.container}>
+     <Image 
+        source={img}
+        style={styles.itemImage}
+     />
+     <Text style={styles.itemText}>
+        {filterDesc(children)}
+     </Text>
+     <Text style={styles.itemPrice}>
+        {cost}
+     </Text>
+   </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: '2%',
+    width: '90%',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  itemImage: {
+    width: 165,
+    height: 165,
+  },
+  itemText: {
+    fontSize: 16
+  },
+  itemPrice: {
+    fontSize: 16,
+    opacity: 0.4
+  }
+});
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons';
 
+import List from '../../components/List';
+
 export default function Home() {
   return (
     <View style={styles.container}>  
@@ -13,9 +15,9 @@ export default function Home() {
       </View>
 
       <View style={styles.optionsCatalogContainer}>
-        <Text style={styles.optionsCatalogText}>TÊNIS</Text>
-        <Text style={[styles.optionsCatalogText, {color: '#cececf'}]}>●</Text>
-        <Text style={[styles.optionsCatalogText, {color: '#cececf'}]}>MASCULINO</Text>
+        <Text style={styles.text}>TÊNIS</Text>
+        <Text style={[styles.text, {color: '#cececf'}]}>●</Text>
+        <Text style={[styles.text, {color: '#cececf'}]}>MASCULINO</Text>
         <TouchableOpacity style={styles.filterButton}>
           <MaterialIcons 
             name="filter-list"
@@ -26,6 +28,37 @@ export default function Home() {
       </View>
 
       <View style={styles.line}/>
+                 
+      <ScrollView>
+        <Text style={[styles.text, {paddingLeft: 6}]}>LANÇAMENTOS</Text>
+
+        <View style={styles.itensList}>
+          <List img={require("../../assets/1.png")} cost="R$259,90">
+            Nike Air Shock
+          </List>
+          <List img={require("../../assets/2.png")} cost="R$289,90">
+            Nike Futemax
+          </List>
+        </View>
+
+        <View style={styles.itensList}>
+          <List img={require("../../assets/3.png")} cost="R$139,90">
+            Adidas Dominator
+          </List>
+          <List img={require("../../assets/4.png")} cost="R$350,90">
+            Adias paraguaio
+          </List>
+        </View>
+
+        <View style={styles.itensList}>
+          <List img={require("../../assets/5.png")} cost="R$190,00">
+            Adidas Fender
+          </List>
+          <List img={require("../../assets/6.png")} cost="R$215,99">
+            Adias ShowRunner
+          </List>
+        </View>
+      </ScrollView>
 
     </View>
   )
@@ -53,7 +86,7 @@ const styles = StyleSheet.create({
     marginVertical: '5%',
     marginHorizontal: '5%'
   },
-  optionsCatalogText: {
+  text: {
     fontFamily: 'Anton_400Regular',
     fontSize: 26,
     marginHorizontal: '1%'
@@ -61,5 +94,9 @@ const styles = StyleSheet.create({
   line: {
     borderBottomColor: '#d8d8d8',
     borderBottomWidth: 2,
+  },
+  itensList: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   }
 });
