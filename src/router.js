f23fa053db697584167098afdef5a943ec0e8731
danielVFS <danielv759153@gmail.com>
2020-08-25 19:10:21 -0300

    add icon cart to detail screen

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Feather } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
 
 import Home from './pages/Home';
 import Details from './pages/Details';
@@ -19,6 +21,17 @@ function Routes() {
         <Stack.Screen 
           name="Details" 
           component={Details}
+          options={{
+            headerRight: () => (
+              <TouchableOpacity style={{ marginRight: 15 }}>
+                <Feather 
+                  name="shopping-cart"
+                  size={24}
+                  color="black"
+                />
+              </TouchableOpacity>
+            )
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
